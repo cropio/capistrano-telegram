d045a96aa04495b149dea82bd47445fb52619b3e
Vitaly Kurennov <vkurennov@gmail.com>
2023-08-31 12:51:56 +0300

    Fix bug and refactoring

--- lib/capistrano/messaging/telegram.rb
+++ lib/capistrano/messaging/telegram.rb
@@ -8,20 +8,35 @@ module Capistrano
       extend Forwardable
       def_delegators :env, :fetch
 
-
-      DEFAULT_MESSAGES = {
-          updating: "#{deployer} has started deploying branch #{branch} of #{application} to #{stage}",
-          reverting: "#{deployer} has started rolling back branch #{branch} of #{application} to #{stage}",
-          updated: "#{deployer} has finished deploying branch #{branch} of #{application} to #{stage}",
-          reverted: "#{deployer} has finished rolling back branch of #{application} to #{stage}",
-          failed: "#{deployer} has failed to #{deploying? ? 'deploy' : 'rollback'} branch #{branch} of #{application} to #{stage}"
-      }
-
       def payload_for(action)
-        text = fetch("message_for_#{action}".to_sym) || DEFAULT_MESSAGES[action]
-        { text: text, parse_mode: 'markdown' }
+        message_type = "message_for_#{action}"
+        {
+            text: fetch(message_type.to_sym) || send(message_type),
+            parse_mode: 'markdown'
+        }
       end
 
+      private
+
+      def message_for_updating
+        "#{deployer} has started deploying branch #{branch} of #{application} to #{stage}"
+      end
+
+      def message_for_reverting
+        "#{deployer} has started rolling back branch #{branch} of #{application} to #{stage}"
+      end
+
+      def message_for_updated
+        "#{deployer} has finished deploying branch #{branch} of #{application} to #{stage}"
+      end
+
+      def message_for_reverted
+        "#{deployer} has finished rolling back branch of #{application} to #{stage}"
+      end
+
+      def message_for_failed
+        "#{deployer} has failed to #{deploying? ? 'deploy' : 'rollback'} branch #{branch} of #{application} to #{stage}"
+      end
     end
   end
 end
