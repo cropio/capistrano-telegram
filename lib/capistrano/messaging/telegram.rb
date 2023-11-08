48c20f06108468e75acc1a4e57111a224cd43b3c
Vadym Yevsieiev <sscode@gmail.com>
2023-11-08 18:22:30 +0200

    feat: make stage bold

--- lib/capistrano/messaging/telegram.rb
+++ lib/capistrano/messaging/telegram.rb
@@ -19,23 +19,23 @@ module Capistrano
       private
 
       def message_for_updating
-        "\u{1F6A2} \u{1F6A2} \u{1F6A2} #{deployer} has started deploying branch #{branch} of #{application} to #{stage}"
+        "\u{1F6A2} \u{1F6A2} \u{1F6A2} #{deployer} has started deploying branch #{branch} of #{application} to *#{stage}*"
       end
 
       def message_for_reverting
-        "#{deployer} has started rolling back branch #{branch} of #{application} to #{stage}"
+        "#{deployer} has started rolling back branch #{branch} of #{application} to *#{stage}*"
       end
 
       def message_for_updated
-        "\u{2705} \u{2705} \u{2705} #{deployer} has finished deploying branch #{branch} of #{application} to #{stage}"
+        "\u{2705} \u{2705} \u{2705} #{deployer} has finished deploying branch #{branch} of #{application} to *#{stage}*"
       end
 
       def message_for_reverted
-        "#{deployer} has finished rolling back branch of #{application} to #{stage}"
+        "#{deployer} has finished rolling back branch of #{application} to *#{stage}*"
       end
 
       def message_for_failed
-        "\u{1F631} \u{1F631} \u{1F631} #{deployer} has failed to #{deploying? ? 'deploy' : 'rollback'} branch #{branch} of #{application} to #{stage}"
+        "\u{1F631} \u{1F631} \u{1F631} #{deployer} has failed to #{deploying? ? 'deploy' : 'rollback'} branch #{branch} of #{application} to *#{stage}*"
       end
     end
   end
